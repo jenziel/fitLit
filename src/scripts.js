d078a874f34729c5f473b0a05ab001e4db49c02a
Jocelyn Wensloff <jewensloff@gmail.com>
2023-07-09 15:27:08 -0600

    fix: Fix the file paths

--- src/scripts.js
+++ src/scripts.js
@@ -1,9 +1,11 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
-import  users  from './data/users';
-// const { users } = require('./data/users');
 
-// console.log("User Data:", users.users);
+// imports 
+import  users  from './data/users';
+import './css/styles.css';
+import './images/turing-logo.png';
+import { exampleFunction1, exampleFunction2  } from './domUpdates';
 
 
 const retrieveUserData = ((userID) => {
@@ -24,19 +26,23 @@ const returnAverageSteps = (() => {
 
 });
 
+// // event handlers 
+// window.addEventListener('load', updateUserGreeting)
+
 // An example of how you tell webpack to use a CSS file
-// import './css/styles.css';
+
+
+
+
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-// import './images/turing-logo.png';
 
 // An example of how you tell webpack to use a JS file
 
 // Example of one way to import functions from the domUpdates file.  You will delete these examples.
-// import { exampleFunction1, exampleFunction2 } from './domUpdates';
 
-// exampleFunction1('Travis');
-// exampleFunction2('Travis')
+exampleFunction1('Travis');
+exampleFunction2('Travis');
 
 export {
   retrieveUserData, 
