aa8e8bdfc1eb7f9c8d700b4a6b7df370efe341fe
Jocelyn Wensloff <jewensloff@gmail.com>
2023-07-09 12:33:03 -0600

    feat: Add a function to return user's data based on user id

--- src/scripts.js
+++ src/scripts.js
@@ -1,19 +1,32 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
+import  users  from './data/users';
+// const { users } = require('./data/users');
 
+console.log("User Data:", users.users);
+
+
+const retrieveUserData = ((userID) => {
+
+  var user = users.users.find(user => user.id === userID) 
+  return user
+
+});
 
 // An example of how you tell webpack to use a CSS file
-import './css/styles.css';
+// import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png';
+// import './images/turing-logo.png';
 
 // An example of how you tell webpack to use a JS file
-import userData from './data/users';
-console.log("User Data:", userData);
 
 // Example of one way to import functions from the domUpdates file.  You will delete these examples.
-import { exampleFunction1, exampleFunction2 } from './domUpdates';
+// import { exampleFunction1, exampleFunction2 } from './domUpdates';
 
-exampleFunction1('Travis');
-exampleFunction2('Travis')
+// exampleFunction1('Travis');
+// exampleFunction2('Travis')
+
+export {
+  retrieveUserData
+}
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,7 +1,21 @@
-import { expect } from 'chai';
+const { expect } = require('chai');
+// const expect = chai.expect;
+// import { expect } from 'chai';
+import {  retrieveUserData } from '../src/scripts';
 
 describe('User Repository', () => {
+
   it('should run tests', function () {
-    expect(true).to.be(true);
+    expect(true).to.be.true;
   });
+
+  it('should be a function', function() {
+    expect(retrieveUserData).to.be.a('function')
+  });
+
+  it('should return a user object', function() {
+    const user2 = retrieveUserData(2)
+    expect(user2.name).to.equal('Tyreek VonRueden')
+  });
+
 });
