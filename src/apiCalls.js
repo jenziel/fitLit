4b0ef6af6b7961cea5fd9fd65433a023f13a9026
Jocelyn Wensloff <jewensloff@gmail.com>
2023-07-12 15:12:47 -0600

    feat: Create in progress fetch call for user data'

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,3 +3,11 @@
 
 console.log('I will be a fetch request!')
 
+
+// user data
+fetch('https://fitlit-api.herokuapp.com/api/v1/users')
+  .then(response => response.json()) 
+  .then(data => {
+
+    console.log('the data is here -> ', data)
+  });
