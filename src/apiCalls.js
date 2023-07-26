188ebdff487318903d50fc30b4be5c58fcf2da1c
Jocelyn Wensloff <jewensloff@gmail.com>
2023-07-25 18:12:05 -0600

    feature: Update URL's and add useer inpur for hydratin data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,9 @@
 // Fetch requests
 
-const userURL = 'https://fitlit-api.herokuapp.com/api/v1/users';
-const hydroURL = 'https://fitlit-api.herokuapp.com/api/v1/hydration';
-const sleepURL = 'https://fitlit-api.herokuapp.com/api/v1/sleep';
-const activityURL = 'https://fitlit-api.herokuapp.com/api/v1/activity';
+const userURL = 'http://localhost:3001/api/v1/users';
+const hydroURL = 'http://localhost:3001/api/v1/hydration';
+const sleepURL = 'http://localhost:3001/api/v1/sleep';
+const activityURL = 'http://localhost:3001/api/v1/activity	';
 
 const urlArray = [userURL, hydroURL, sleepURL, activityURL];
 
