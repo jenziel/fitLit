0ed459e9ce3a8eaf31d0c36cc53237d3f4f9ba38
Jocelyn Wensloff <jewensloff@gmail.com>
2023-07-30 16:37:34 -0600

    refactor: Add some last minute html changes for the screen reader

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,3 @@
-
-
 const userURL = 'http://localhost:3001/api/v1/users';
 const hydroURL = 'http://localhost:3001/api/v1/hydration';
 const sleepURL = 'http://localhost:3001/api/v1/sleep';
@@ -8,35 +6,30 @@ const activityURL = 'http://localhost:3001/api/v1/activity	';
 const urlArray = [userURL, hydroURL, sleepURL, activityURL];
 
 // GET requests
-  export const createFetchRequest = () => {
-    return urlArray.map(url => 
+export const createFetchRequest = () => {
+  return urlArray.map((url) =>
     fetch(url)
-    .then(response => response.json())
-    .catch(error => console.log(error))
-    )
-  }
+      .then((response) => response.json())
+      .catch((error) => console.log(error))
+  );
+};
 
-  // POST request 
+// POST request
 export const postUserInput = (currentUser, input) => {
   let data = {
     userID: currentUser.id,
     date: '2023/07/02',
-    numOunces: input
+    numOunces: input,
   };
 
   return fetch(hydroURL, {
     method: 'POST',
     body: JSON.stringify(data),
     headers: {
-      'Content-Type': 'application/json'
-    }
-  }) 
-  .catch(error => {
+      'Content-Type': 'application/json',
+    },
+  }).catch((error) => {
     console.log(error);
     throw error;
   });
 };
-
-
-
-
